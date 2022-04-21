f8090271ba5af41dfb980706d9014c33ab5b0df1
abuobaid9 <abuobaid.anas1999@gmail.com>
2022-04-21 19:04:20 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -1,51 +1,46 @@
-'use strict';
+"use strict";
 
-
-function Employee(employee_id,full_Name,department,level,imag,salary){
+let allEmployee =[];
+function Employee(employee_id,full_Name,department,level,imag){
     this.employee_id=employee_id;
     this.full_Name=full_Name;
     this.department=department;
     this.level=level;
     this.image_URL=imag;
-    this.salary=salary;
+    this.salary=0;
+    allEmployee.push(this);
 }
 
-let ghazi= new Employee("1000" ,"Ghazi Samer","Administration","Senior");
-let lana= new Employee("1001" ,"Lana Ali","Finance","Senior");
-let tamara = new Employee("1002","Tamara Ayoub","Marketing","Senior");
-let saif = new Employee("1003","Safi Walid","Administration","Mid-Senior");
-let omar = new Employee("1004","Omar Zaid","Development","Senior");
- let rana= new Employee("1005","Rana Saleh","Development","Junior");
- let hadi =new Employee("1006","Hadi Ahmad","Finance","Mid-Senior");
+let ghazi= new Employee(1000 ,"Ghazi Samer","Administration","senior");
+let lana= new Employee(1001,"Lana Ali","Finance","senior");
+let tamara = new Employee(1002,"Tamara Ayoub","Marketing","senior");
+let saif = new Employee(1003,"Safi Walid","Administration","mid-senior");
+let omar = new Employee(1004,"Omar Zaid","Development","senior");
+ let rana= new Employee(1005,"Rana Saleh","Development","junior");
+ let hadi =new Employee(1006,"Hadi Ahmad","Finance","mid-senior");
 
-Employee.prototype.salary=function(){
-if(this.level == "junior"){
-    let junior_Salary=Math.floor(Math.random() * (1000 - 500)) + 500;
-    let grossSalary = junior_Salary;
-    let incomeTax = grossSalary- (junior_Salary * (7.5/100));
-    return incomeTax;
+
+Employee.prototype.newSalary=function(){
+if(this.level==="junior"){
+    this.salary=getRndInteger(500, 1000);
+    this.salary=this.salary * (7.5/100); 
 }
-
-else if(this.level =="mid-senior") {
-    let mid_Salary = Math.floor(Math.random() * (1500 - 1000)) + 1000;
-    let grossSalary = mid_Salary;
-    let incomeTax2 = grossSalary- (mid_Salary * (7.5/100));
-    return incomeTax2;
+ if(this.level==="mid-senior") {
+    this.salary=getRndInteger(1000, 1500);
+    this.salary=this.salary * (7.5/100);
 }
-
-else if(mid-Senior=="senior"){
-    let senior_Salary=Math.floor(Math.random() * (2000 - 1500)) + 1500;
-    let grossSalary = senior_Salary;
-    let incomeTax3 = grossSalary- (senior_Salary * (7.5/100));
-    return incomeTax3;
-}
-else{}
-
+if(this.level==="senior"){
+    this.salary=getRndInteger(1500, 2000);
+    this.salary=this.salary * (7.5/100);
+  }
 }
 Employee.prototype.render =function(){
-    console.log(this.full_Name);
-    console.log(this.salary);
-    console.log(this.department);
 document.write(`<h1> My name is ${this.full_Name} ,and my salary is ${this.salary}</h1>`);
 }
-ghazi.render();
+for (let i = 0; i < allEmployee.length; i++) {
+    allEmployee[i].newSalary();
+    allEmployee[i].render();
+}
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
