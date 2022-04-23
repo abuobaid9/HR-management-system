630ff3062ca52a73fbdf6d566f3e42d719199f95
abuobaid9 <abuobaid.anas1999@gmail.com>
2022-04-23 23:42:18 +0300

    add

--- js/app.js
+++ js/app.js
@@ -1,46 +1,97 @@
 "use strict";
-
+let form= document.getElementById("formId");
+let sectionEl = document.getElementById("separatId");
 let allEmployee =[];
-function Employee(employee_id,full_Name,department,level,imag){
-    this.employee_id=employee_id;
+
+
+
+
+
+
+//constructor
+function Employee(employee_id,full_Name,department,level,imag,salary){
+    this.employee_id=getrandom();
     this.full_Name=full_Name;
     this.department=department;
     this.level=level;
     this.image_URL=imag;
-    this.salary=0;
+    this.salary=salary;
+
     allEmployee.push(this);
+
 }
 
-let ghazi= new Employee(1000 ,"Ghazi Samer","Administration","senior");
-let lana= new Employee(1001,"Lana Ali","Finance","senior");
-let tamara = new Employee(1002,"Tamara Ayoub","Marketing","senior");
-let saif = new Employee(1003,"Safi Walid","Administration","mid-senior");
-let omar = new Employee(1004,"Omar Zaid","Development","senior");
- let rana= new Employee(1005,"Rana Saleh","Development","junior");
- let hadi =new Employee(1006,"Hadi Ahmad","Finance","mid-senior");
 
-
-Employee.prototype.newSalary=function(){
-if(this.level==="junior"){
-    this.salary=getRndInteger(500, 1000);
-    this.salary=this.salary * (7.5/100); 
-}
- if(this.level==="mid-senior") {
-    this.salary=getRndInteger(1000, 1500);
-    this.salary=this.salary * (7.5/100);
-}
-if(this.level==="senior"){
-    this.salary=getRndInteger(1500, 2000);
-    this.salary=this.salary * (7.5/100);
-  }
-}
+//method
 Employee.prototype.render =function(){
-document.write(`<h1> My name is ${this.full_Name} ,and my salary is ${this.salary}</h1>`);
+    let images =document.createElement("img");
+    images.src=this.image_URL;
+    sectionEl.appendChild(images);
+    
+          let employeeID= document.createElement("h4");
+         employeeID.textContent=` ID: ${this.employee_id}`;
+        sectionEl.appendChild(employeeID);
+
+
+let name =document.createElement("h3");
+name.textContent=` Name : ${this.full_Name}`;
+sectionEl.appendChild(name);
+       
+   
+        let depart =document.createElement("h4");
+        depart.textContent=`Department : ${this.department}`;
+        sectionEl.appendChild(depart);
+
+ let leveles =document.createElement("h4");
+ leveles.textContent=`Level : ${this.level}`;
+ sectionEl.appendChild(leveles);
+         
+
+        
+
+let salarySec =document.createElement("p");
+salarySec.textContent=`Salary :${this.salary} JD`;
+sectionEl.appendChild(salarySec);   
+
+
 }
+
+
+let ghazi= new Employee(1,"Ghazi Samer","Administration","senior","./img/Ghazi.jpg",1800);
+let lana= new Employee(1001,"Lana Ali","Finance","senior","./img/Lana.jpg",2000);
+let tamara = new Employee(1002,"Tamara Ayoub","Marketing","senior","./img/Tamara.jpg",1600);
+let saif = new Employee(1003,"Safi Walid","Administration","mid-senior","./img/Safi.jpg",1200);
+let omar = new Employee(1004,"Omar Zaid","Development","senior","./img/Omar.jpg",1700);
+let rana= new Employee(1005,"Rana Saleh","Development","junior","./img/Rana.jpg",650);
+let hadi =new Employee(1006,"Hadi Ahmad","Finance","mid-senior","./img/Hadi.jpg",1350);
+
+console.log(allEmployee);
+
+//  ghazi.render();
+
 for (let i = 0; i < allEmployee.length; i++) {
-    allEmployee[i].newSalary();
     allEmployee[i].render();
 }
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+
+form.addEventListener("submit",handleSubmit);
+
+// finction
+function getrandom (){
+    let val = Math.floor(1000 + Math.random() * 9000);
+return val;
+}   
+
+function handleSubmit(event){
+    event.preventDefault();
+ 
+    // let pesronID=event.target.pesronID.value;
+    let fname=event.target.fname.value;
+    let department=event.target.department.value;
+    let level = event.target.level.value;
+    let imag = event.target.imag.value;
+    // let personSalary = event.target.personSalary.value;
+
+    let newinfo=new Employee (fname,department,level,imag);
+    newinfo.render();
+    console.log(newinfo);
+}
